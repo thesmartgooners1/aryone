5410c4f850426598dbaefe19c20991e8195b8563
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-08-05 00:39:18 +0700

    Delete games.js

--- games.js
+++
@@ -1,6 +0,0 @@
-// အမြဲတမ်းပြချင်သော ပွဲများကို ဤနေရာတွင် ထည့်ပါ။ အသုံးပြုခွင့်ရှိသော stream URL များသာ သုံးပါ။
-window.ARYONE_DEFAULT_GAMES=[
-  {id:'basket-demo',name:'Basketball Live Demo',team1:'AR YONE Blue',team2:'AR YONE Red',sport:'ဘတ်စကက်ဘော',time:'LIVE NOW',image:'',url:'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'},
-  {id:'tennis-demo',name:'Tennis Live Demo',team1:'Player A',team2:'Player B',sport:'တင်းနစ်',time:'LIVE NOW',image:'',url:'https://test-streams.mux.dev/test_001/stream.m3u8'},
-  {id:'volley-demo',name:'Volleyball Live Demo',team1:'Team Yangon',team2:'Team Mandalay',sport:'ဘော်လီဘော',time:'ယနေ့ 8:00 PM',image:'',url:'https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8'}
-];
