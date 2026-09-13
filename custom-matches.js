eaa81c9aed742eaa15457d78ead160f2e9d7c105
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-09-13 21:04:11 +0700

    Update custom-matches.js

--- custom-matches.js
+++ custom-matches.js
@@ -76,7 +76,16 @@ window.ARYONE_FOOTBALL_MATCHES = [
     awayLogo: "https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg",
     kickoff: "2026-09-13T20:45:00+06:30",
     status: "auto",
-    streams: []
+    streams: [
+      {
+        label: "HD",
+        url: "https://live05.meung.app/live/75748097.m3u8"
+      },
+      {
+        label: "HD-2",
+        url: "https://live05.meung.app/live/97885009.m3u8"
+      }
+    ]
   },
 
   {
