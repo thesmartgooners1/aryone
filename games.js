5d9388257ec4ab0771797389a61cd79c0cd9e2ba
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-09-20 01:24:27 +0700

    Update games.js

--- games.js
+++ games.js
@@ -1,3 +1,3 @@
 // အမြဲတမ်းပြချင်သော ပွဲများကို ဤနေရာတွင် ထည့်ပါ။ အသုံးပြုခွင့်ရှိသော stream URL များသာ သုံးပါ။
 window.ARYONE_DEFAULT_GAMES=[
-  {id:'basket-demo',name:'Basketball Live Demo',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC 331',time:'LIVE NOW',image:'',url:'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'} ];
+  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'} ];
