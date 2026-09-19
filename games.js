b7eeee55e712a64a817ec4afeb89d2bc1713c32b
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-09-20 05:34:31 +0700

    Add new game entry for UFC 331

--- games.js
+++ games.js
@@ -1,3 +1,3 @@
 // အမြဲတမ်းပြချင်သော ပွဲများကို ဤနေရာတွင် ထည့်ပါ။ 
 window.ARYONE_DEFAULT_GAMES=[
-  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'https://viva.vivatops.store/simpch1/index.m3u8?token=eyJzdHJlYW0iOiJzaW1wY2gxIiwiaXAiOiIwLjAuMC4wIiwiZXhwIjoxNzg5ODU3MDE1LCJyYW5kIjoiZWY0NThjNGRiYjU2NWNlYyJ9.f25face7c68fa1d1708269eb5641fc253e3daa55bcdbc55a05af74922ae8297e'} ];
+  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'https://viva.vivatops.store/simpch1/index.m3u8?token=eyJzdHJlYW0iOiJzaW1wY2gxIiwiaXAiOiIwLjAuMC4wIiwiZXhwIjoxNzg5ODU4OTM5LCJyYW5kIjoiNjEwZGRkODYzOGViZDM3ZSJ9.e55b351095a8ef07a1489b6b534ca9be1ad4136be611460f53deb9554f6f9ed2'} ];
