8576616b4cd6ba5a665989555d1621946e95d426
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-09-20 03:47:37 +0700

    Change stream URL for UFC 331 game
    Updated the stream URL for the Ultimate Fighting Championship 331 game.

--- games.js
+++ games.js
@@ -1,3 +1,3 @@
-// အမြဲတမ်းပြချင်သော ပွဲများကို ဤနေရာတွင် ထည့်ပါ။ အသုံးပြုခွင့်ရှိသော stream URL များသာ သုံးပါ။
+// အမြဲတမ်းပြချင်သော ပွဲများကို ဤနေရာတွင် ထည့်ပါ။ 
 window.ARYONE_DEFAULT_GAMES=[
-  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'} ];
+  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'https://viva.vivatops.store/simpch1/index.m3u8?token=eyJzdHJlYW0iOiJzaW1wY2gxIiwiaXAiOiIwLjAuMC4wIiwiZXhwIjoxNzg5ODUyMjYxLCJyYW5kIjoiYjhhNzFmZGU2YjVhZTFlYyJ9.f3bd73bec173852eb1bc1b9ec15822c5527a771a1b834c0543aabf2f90bde1a2'} ];
