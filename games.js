e0b511d0112fb11fa0712f505722ab2535f3f3ba
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-09-20 10:29:20 +0700

    Update games.js

--- games.js
+++ games.js
@@ -1,3 +1,3 @@
 // အမြဲတမ်းပြချင်သော ပွဲများကို ဤနေရာတွင် ထည့်ပါ။ 
 window.ARYONE_DEFAULT_GAMES=[
-  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'https://zb-play.wjhwpd.com/fqzb/576490511_1789870045_gqzm.m3u8?txSecret=d1f43c8ae83ca06ea4577df9d60147df&txTime=6AAF516C&token=4dcfb9e3ddde271d&device=3&platform=fqzb'} ];
+  {id:'basket-demo',name:'Ultimate Fighting Championship 331',team1:'Joshua Van',team2:'Alexandre Pantoja',sport:'UFC',time:'LIVE NOW',image:'',url:'	https://zb-play.wjhwpd.com/fqzb/576490511_1789870045_gqzm.m3u8?txSecret=f63b70859a9766cfb133292db53eec53&txTime=6AAF5EF4&token=c77506c3195ef905&device=3&platform=fqzb'} ];
